79300f99f10dd0eb9e0459a5a6d8b1844e30b420
Justin Phelps & Tyler Adams <pair+justinphelps.tyleraadams@devbootcamp.com>
2014-04-25 16:20:30 -0400

    added signup erb, tweaked css

--- source/flashcards/db/seeds_users.rb
+++ source/flashcards/db/seeds_users.rb
@@ -1,5 +1,5 @@
 10.times do
-  fusername = Faker::Name.first_name + Faker::Name.last_name
+  fusername = (Faker::Name.first_name + Faker::Name.last_name).downcase
   femail = Faker::Internet.free_email
   User.create(username: fusername, email: femail, password: 'lol' )
 end
