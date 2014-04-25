f960e9e297c22f7af0e196acf2b244a16857e538
Johnathan Weisner <null>
2014-04-25 16:24:16 -0400

    Fixed a merge artifact in seeds

--- source/flashcards/db/seeds.rb
+++ source/flashcards/db/seeds.rb
@@ -2,6 +2,4 @@ Deck.transaction do
   require_relative 'seeds_round'
   require_relative 'seeds_cards_and_decks.rb'
   require_relative 'seeds_users'
-  deck = Deck.create(name: "Math Problems", genre: "Math")
-  card = Card.new
 end
