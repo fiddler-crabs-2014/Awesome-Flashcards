1fb3d0e681c3c58c81792bbdef826eada703c9bc
Johnathan Weisner <null>
2014-04-25 16:14:07 -0400

    Fixed seeds

--- source/flashcards/db/seeds.rb
+++ source/flashcards/db/seeds.rb
@@ -1,7 +1,7 @@
-require_relative 'seeds_round'
-require_relative 'seeds_cards_and_decks.rb'
-require_relative 'seeds_users'
-deck = Deck.create(name: "Math Problems", genre: "Math")
-card = Card.new
 Deck.transaction do
+  require_relative 'seeds_round'
+  require_relative 'seeds_cards_and_decks.rb'
+  require_relative 'seeds_users'
+  deck = Deck.create(name: "Math Problems", genre: "Math")
+  card = Card.new
 end
