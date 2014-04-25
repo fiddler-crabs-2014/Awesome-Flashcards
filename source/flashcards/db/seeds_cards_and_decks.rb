c15001edc6295c3770163047eb168e3b654cd187
Johnathan Weisner <null>
2014-04-25 16:21:11 -0400

    Fixed seeding once more.

--- source/flashcards/db/seeds_cards_and_decks.rb
+++ source/flashcards/db/seeds_cards_and_decks.rb
@@ -28,7 +28,7 @@ Deck.transaction do
   10.times do
     card = Card.new
     num1,num2,num3 = rand(10)+1,rand(10)+1,rand(30)+1
-    answer = num1 * num2
+    answer = num1 * num2 * num3
     card.question = "#{num1} x #{num2} x #{num3}= ?"
     card.answer = "#{answer}"
     card.difficulty_score = 3
