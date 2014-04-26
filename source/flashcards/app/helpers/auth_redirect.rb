de87ab4335515579e7ee2f5f5ad2cab9269d3801
Justin Phelps & Tyler Adams <pair+justinphelps.tyleraadams@devbootcamp.com>
2014-04-26 14:58:28 -0400

    auth_redirect helper added

---
+++ source/flashcards/app/helpers/auth_redirect.rb
@@ -0,0 +1,11 @@
+def auth_redirect(redir = nil)
+  if User.authenticate(params[:username], params[:password])
+    @user = User.where(username: params[:username])[0]
+    session[:user_id] = @user.id
+    flashlogger("auth_redirect redir: #{redir}")
+    redirect redir if redir != nil
+  else
+    flashlogger("auth_redirect failed auth redir /signin")
+    redirect '/signin'
+  end
+end
